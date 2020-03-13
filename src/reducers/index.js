50583dc8dc0540d2d50bd03afa21ee27a2629045
Jessica <jessica.ho.jh@gmail.com>
2020-03-12 23:41:18 -0700

    added bootstrap CDN

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import searchReducer from './searchReducer';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default combineReducers({
   search: searchReducer
 });
